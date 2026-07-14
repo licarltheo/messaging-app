34b7ffb66c4eb09a4149543104d507ce88ac1c02
licarltheo <licarltheo@gmail.com>
2026-07-14 14:42:39 +0000

    feat: add backend/models/Group.js

---
+++ backend/models/Group.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const groupSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  members: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  admin: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Group', groupSchema);
